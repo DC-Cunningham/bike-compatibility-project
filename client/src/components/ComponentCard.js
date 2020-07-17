4158ccbdc32d20065327464d20c2052878fbf77a
DCRevResLabs <dc@revolutionresearch.com.au>
2020-07-17 10:17:34 +1000

    removed redundant import reference

--- client/src/components/ComponentCard.js
+++ client/src/components/ComponentCard.js
@@ -10,11 +10,9 @@ import {
   makeStyles,
 } from "@material-ui/core";
 import Card from "@material-ui/core/Card";
-import CardMedia from "@material-ui/core/CardMedia";
 import CardContent from "@material-ui/core/CardContent";
 import Divider from "@material-ui/core/Divider";
 import Typography from "@material-ui/core/Typography";
-import Icons from "../assets/images";
 
 const useStyles = makeStyles((theme) => ({
   card: {
